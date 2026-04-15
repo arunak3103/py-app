13db644cbacf7a5d43f78af8b2d172b88a2e14c6
arunak3103 <arunakj3198@gmail.com>
2026-04-15 15:13:35 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello for python"
+    return "Hello for python by arun"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
